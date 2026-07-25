67181c1529a26a88ac83b14cb7c3fdc099f8bcf7
Tacio Jesus <jesustacio@gmail.com>
2026-07-25 18:19:42 -0300

    Center page headlines

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Manrope, Playfair_Display } from "next/font/google";
 import { headers } from "next/headers";
 import "./globals.css";
+import "./headlines.css";
 
 const manrope = Manrope({ variable: "--font-sans", subsets: ["latin"] });
 const playfair = Playfair_Display({ variable: "--font-serif", subsets: ["latin"], style: ["normal", "italic"] });
