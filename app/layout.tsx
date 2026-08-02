bb0bface9440178c6ff4ab943bb9d2b92e97d115
Tacio Jesus <jesustacio@gmail.com>
2026-08-02 18:04:08 -0300

    Remove UTMify pixel script

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,6 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return <html lang="pt-BR"><head>
-    <script dangerouslySetInnerHTML={{ __html: 'window.pixelId="6a668772849ca51086a296f7";' }} />
-    <script src="https://cdn.utmify.com.br/scripts/pixel/pixel.js" async defer />
     <script src="https://cdn.utmify.com.br/scripts/utms/latest.js" data-utmify-prevent-xcod-sck="" data-utmify-prevent-subids="" async defer />
     <script data-goatcounter="https://enfermeiro.goatcounter.com/count" src="https://gc.zgo.at/count.js" async />
   </head><body>{children}</body></html>;
